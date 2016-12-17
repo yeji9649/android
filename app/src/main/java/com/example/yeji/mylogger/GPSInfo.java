2a07c1f33965516b4edab7eed0c5d297ff93f860
kim yeji <kmjiyu@kookmin.ac.kr>
2016-12-17 15:36:24 +0900

    first upload

--- app/src/main/java/com/example/yeji/mylogger/GPSInfo.java
+++ app/src/main/java/com/example/yeji/mylogger/GPSInfo.java
@@ -1,52 +1,65 @@
 package com.example.yeji.mylogger;
 
 /**
- * Created by yeji on 2016-12-01.
+ * Created by yeji on 2016-12-17.
  */
 
+import android.app.AlertDialog;
+import android.app.Service;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.content.pm.PackageManager;
+import android.location.Location;
 import android.location.LocationListener;
+import android.location.LocationManager;
 import android.os.Bundle;
 import android.os.IBinder;
 import android.provider.Settings;
-import android.location.Location;
-import android.location.LocationManager;
-import android.location.OnNmeaMessageListener;
-import android.content.Intent;
-import android.content.Context;
-import android.app.Service;
 import android.support.v4.app.ActivityCompat;
 
-public class GPSInfo extends Service implements LocationListener {
+public class GpsInfo extends Service implements LocationListener {
+
     private final Context mContext;
 
+    // 현재 GPS 사용유무
     boolean isGPSEnabled = false;
-    boolean isNetworkEnabled = false;
-    boolean isGetLocation = false;
-    Location location;
-    double lat = 0;//위도
-    double lon = 0;//경도
-    private static final long UPDATE_TIME = 10;//업데이트 되는 시간 설정
-    private static final long UPDATE_DISTANCE = 100;//업데이트되는 거리설정
 
-    public GPSInfo(Context context) {
+    // 네트워크 사용유무
+    boolean isNetworkEnabled = false;
+
+    // GPS 상태값
+    boolean isGetLocation = false;
+
+    Location location;
+    double lat; // 위도
+    double lon; // 경도
+
+    // GPS 정보 업데이트 거리 10미터
+    private static final long MIN_DISTANCE_UPDATES = 10;
+
+    // GPS 정보 업데이트 시간 1/1000
+    private static final long MIN_TIME_UPDATES = 1000 * 60 * 1;
+
+    protected LocationManager locationManager;
+
+    public GpsInfo(Context context) {
         this.mContext = context;
         getLocation();
     }
-
-    protected LocationManager locationManager;//위치서비스를 제공하는 서비스를 가져오는 역할을 한다
 
     public Location getLocation() {
         try {
             locationManager = (LocationManager) mContext
                     .getSystemService(LOCATION_SERVICE);
+
             isGPSEnabled = locationManager
-                    .isProviderEnabled(LocationManager.GPS_PROVIDER);//gps 제공자에게gps가작동하는지아닌지를얻어온다
+                    .isProviderEnabled(LocationManager.GPS_PROVIDER);
+
             isNetworkEnabled = locationManager
-                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);//마찬가지로네트워크서비스제공자에서네트워크가제대로작동하는지안하는지를얻어온다
+                    .isProviderEnabled(LocationManager.NETWORK_PROVIDER);
 
             if (!isGPSEnabled && !isNetworkEnabled) {
-
             } else {
                 this.isGetLocation = true;
                 if (isNetworkEnabled) {
@@ -58,25 +71,32 @@ public class GPSInfo extends Service implements LocationListener {
                         //                                          int[] grantResults)
                         // to handle the case where the user grants the permission. See the documentation
                         // for ActivityCompat#requestPermissions for more details.
-                        return null;
+                        return TODO;
                     }
                     locationManager.requestLocationUpdates(
-                            LocationManager.NETWORK_PROVIDER, UPDATE_TIME,
-                            UPDATE_DISTANCE, this);
+                            LocationManager.NETWORK_PROVIDER,
+                            MIN_TIME_UPDATES,
+                            MIN_DISTANCE_UPDATES, this);
+
                     if (locationManager != null) {
                         location = locationManager
                                 .getLastKnownLocation(LocationManager.NETWORK_PROVIDER);
                         if (location != null) {
+                            // 위도 경도 저장
                             lat = location.getLatitude();
                             lon = location.getLongitude();
-                        }//네크워크서비스가제대로작동해위치를받아오면각각을위도, 경도에저장한다
+                        }
                     }
                 }
+
                 if (isGPSEnabled) {
                     if (location == null) {
-                        locationManager.requestLocationUpdates(
-                                LocationManager.GPS_PROVIDER, UPDATE_TIME,
-                                UPDATE_DISTANCE, this);
+                        locationManager
+                                .requestLocationUpdates(
+                                        LocationManager.GPS_PROVIDER,
+                                        MIN_TIME_UPDATES,
+                                        MIN_DISTANCE_UPDATES,
+                                        this);
                         if (locationManager != null) {
                             location = locationManager
                                     .getLastKnownLocation(LocationManager.GPS_PROVIDER);
@@ -88,56 +108,100 @@ public class GPSInfo extends Service implements LocationListener {
                     }
                 }
             }
+
         } catch (Exception e) {
-            e.printStackTrace();//에러메세지의발생근원지를찾아서단계별로에러를출력한다.
+            e.printStackTrace();
         }
         return location;
     }
-    /*
-    public void stopUsingGPS(){
-        if(locationManager != null) {
-            locationManager.removeUpdates(GPSInfo, this);
-        }
-    }*/
 
-    public double getLatitude() {
+    /**
+     * GPS 종료
+     * */
+    public void stopUsingGPS() {
         if (locationManager != null) {
+            locationManager.removeUpdates(GpsInfo.this);
+        }
+    }
+
+    /**
+     * 위도값
+     * */
+    public double getLatitude() {
+        if (location != null) {
             lat = location.getLatitude();
         }
         return lat;
     }
+
+    /**
+     * 경도값
+     * */
     public double getLongitude() {
-        if (locationManager != null) {
+        if (location != null) {
             lon = location.getLongitude();
         }
         return lon;
     }
-    public boolean isGetLocation(){
+
+    public boolean isGetLocation() {
         return this.isGetLocation;
     }
-    @Override
-    public void onLocationChanged(Location location) {
-        //TODO Auto-generated method stub
+
+    /**
+     * GPS 정보를 가져오지 못했을때 설정값으로 갈지 물어보는 alert 창
+     * */
+    public void showSettingsAlert() {
+        AlertDialog.Builder alertDialog = new AlertDialog.Builder(
+                mContext);
+
+        alertDialog.setTitle("GPS 사용유무셋팅");
+        alertDialog
+                .setMessage("GPS 셋팅이 되지 않았을수도 있습니다.\n 설정창으로 가시겠습니까?");
+
+        alertDialog.setPositiveButton("Settings",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        Intent intent = new Intent(
+                                Settings.ACTION_LOCATION_SOURCE_SETTINGS);
+                        mContext.startActivity(intent);
+                    }
+                });
+        alertDialog.setNegativeButton("Cancel",
+                new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog,
+                                        int which) {
+                        dialog.cancel();
+                    }
+                });
+
+        alertDialog.show();
     }
 
-
     @Override
-    public void onProviderDisabled(String provider) {
-        //TODO Auto-generated method stub
-    }
-
-    @Override
-    public void onProviderEnabled(String provider) {
-        //TODO Auto-generated method stub
-    }
-
-    @Override
-    public void onStatusChanged(String provider, int status, Bundle extras) {
-        //TODO Auto-generated method stub
-    }
-
-    public IBinder onBind(Intent arg0){
+    public IBinder onBind(Intent arg0) {
         return null;
     }
 
+    public void onLocationChanged(Location location) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void onStatusChanged(String provider, int status,
+                                Bundle extras) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void onProviderEnabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
+
+    public void onProviderDisabled(String provider) {
+        // TODO Auto-generated method stub
+
+    }
 }
--- app/src/main/java/com/example/yeji/mylogger/LogActivity.java
+++ app/src/main/java/com/example/yeji/mylogger/LogActivity.java
@@ -3,12 +3,81 @@ package com.example.yeji.mylogger;
 import android.app.Activity;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.graphics.Point;
+import android.support.v4.app.FragmentActivity;
+import android.util.Log;
+import android.content.SharedPreferences.Editor;
 
-public class LogActivity extends Activity {
+import com.google.android.gms.common.GooglePlayServicesUtil;
+import com.google.android.gms.maps.CameraUpdateFactory;
+import com.google.android.gms.maps.GoogleMap;
+import com.google.android.gms.maps.GoogleMap.OnMapClickListener;
+import com.google.android.gms.maps.MapsInitializer;
+import com.google.android.gms.maps.SupportMapFragment;
+import com.google.android.gms.maps.model.BitmapDescriptorFactory;
+import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.maps.model.MarkerOptions;
+
+
+
+public class LogActivity extends FragmentActivity implements OnMapClickListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_log);
+
+    }
+    /** Map 클릭시 터치 이벤트 */
+    public void onMapClick(LatLng point) {
+
+        // 현재 위도와 경도에서 화면 포인트를 알려준다
+        Point screenPt = mGoogleMap.getProjection().toScreenLocation(point);
+
+        // 현재 화면에 찍힌 포인트로 부터 위도와 경도를 알려준다.
+        LatLng latLng = mGoogleMap.getProjection().fromScreenLocation(screenPt);
+
+        Log.d("맵좌표", "좌표: 위도(" + String.valueOf(point.latitude) + "), 경도("
+                + String.valueOf(point.longitude) + ")");
+        Log.d("화면좌표", "화면좌표: X(" + String.valueOf(screenPt.x) + "), Y("
+                + String.valueOf(screenPt.y) + ")");
+    }
+
+    /**
+     * 초기화
+     * @author
+     */
+    private void init() {
+
+        GooglePlayServicesUtil.isGooglePlayServicesAvailable(LogActivity.this);
+        mGoogleMap = ((SupportMapFragment) getSupportFragmentManager()
+                .findFragmentById(R.id.map)).getMap();
+
+        // 맵의 이동
+        //mGoogleMap.moveCamera(CameraUpdateFactory.newLatLngZoom(position, 15));
+
+        GpsInfo gps = new GpsInfo(LogActivity.this);
+        // GPS 사용유무 가져오기
+        if (gps.isGetLocation()) {
+            double latitude = gps.getLatitude();
+            double longitude = gps.getLongitude();
+
+            // Creating a LatLng object for the current location
+            LatLng latLng = new LatLng(latitude, longitude);
+
+            // Showing the current location in Google Map
+            mGoogleMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+
+            // Map 을 zoom 합니다.
+            mGoogleMap.animateCamera(CameraUpdateFactory.zoomTo(13));
+
+            // 마커 설정.
+            MarkerOptions optFirst = new MarkerOptions();
+            optFirst.position(latLng);// 위도 • 경도
+            optFirst.title("Current Position");// 제목 미리보기
+            optFirst.snippet("Snippet");
+            optFirst.icon(BitmapDescriptorFactory.fromResource(R.drawable.ic_launcher));
+            mGoogleMap.addMarker(optFirst).showInfoWindow();
+        }
     }
 }
